19b30b8268d9e45412af60b5bf8cea1c176717bc
Loice-KaniniMwau <mwauloice@gmail.com>
2023-03-29 19:53:25 +0300

    lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,22 +9,23 @@ println( myNames(listOf("maryanne","lucyanne","chepchumba","kennedy","kibet","lu
    println(final)
 
 
-  var car1=MyCars("KBZ123E",34.6)
-    var car2=MyCars("KAB890Z",78.9)
-    var car3=MyCars("KDB890E",67.4)
+    var car1 = AllVehicles("KYF020F",60)
+    var car2 = AllVehicles("KBM230G",50)
+    var car3 = AllVehicles("KCF343P",70)
+    var car4 = AllVehicles("KFD343V",75)
+    var finalcars= listOf(car1,car2,car3,car4)
+    println(carlist(finalcars))
+
+
+    //var finalCars= listOf(car1,car2,car3)
 }
 //Given a list of 10 strings, write a function that returns a list of the strings
 //at even indices i.e index 2,4,6 etc
 fun myNames(allNmaes:List<String>):List<String> {
-    var firstName = 0
+    var firstName =allNmaes.filter { item->allNmaes.indexOf(item)%2==0 &&(allNmaes.indexOf(item))!=0 }
+  return firstName
 
-var finalName= listOf(allNmaes[firstName])
-    when(finalName){
 
-    }
-
-firstName+=2
-return finalName
 }
 //Given a list of people’s heights in metres. Write a function that returns
 //the average height and the total height
@@ -40,7 +41,7 @@ fun averageHeight(allHeight:List<Double>):theHeight{
 //Given a list of Person objects, each with the attributes, name, age,
 //height and weight. Sort the list in order of descending age
  data class Persons(var name:String,var age:Int,var height: Double,var weight:Double)
-   fun allpeople() {
+    fun allpeople() {
     var person = Persons("loice", 21, 5.78, 50.1)
     var person1 = Persons("maryanne", 30, 5.99, 56.1)
     var person2 = Persons("jecinta", 14, 6.4, 56.98)
@@ -54,8 +55,12 @@ fun averageHeight(allHeight:List<Double>):theHeight{
 
 
 
-fun mypeople():Persons{
-  return mypeople()
+
+fun mypeople(name: String,age: Int,height: theHeight,weight: Double):Any{
+   var person3=Persons("kennedy",34,5.6,70.3)
+    var person4=Persons("kibet",32,6.7,50.6)
+    var final= listOf(person3,person4)
+    return final
 
 }
 
@@ -63,15 +68,21 @@ fun mypeople():Persons{
 //Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list.
-data class MyCars(var registration:String,var mileage:Double)
-fun allCars(allVehicles:List<MyCars>):Double{
-var carMileage=allVehicles.sumOf { vehicle->vehicle.mileage }/allVehicles.size
-    return carMileage
-
-
-
-
-
-
+data class AllVehicles(var registration:String, var mileage:Int)
+fun carlist(Cars:List<AllVehicles>):Int{
+    var Myaverage=0
+    Cars.forEach { car->car.mileage
+        Myaverage=car.mileage
+    }
+    var totalAverage=Myaverage/Cars.count()
+    return totalAverage
 }
+
+
+
+
+
+
+
+
 
